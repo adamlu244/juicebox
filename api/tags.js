b061a5f908f9f2f1f62dcefea9d48daa683a50d1
Adam Lu <adamlu09@gmail.com>
2023-01-17 19:23:18 -0800

    jb part3 of part2 done

--- api/tags.js
+++ api/tags.js
@@ -7,7 +7,7 @@ tagsRouter.use((req, res, next) => {
     next();
 });
 
-const { getAllTags } = require("../db");
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.get("/", async (req, res) => {
     const tags = await getAllTags();
@@ -16,5 +16,28 @@ tagsRouter.get("/", async (req, res) => {
         tags
     });
 });
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+    const { tagName } = req.params;
+
+    try {
+        const postByTag = await getPostsByTagName(tagName);
+
+        const posts = postByTag.filter(post => {
+            if (post.active) {
+                return true;
+            }
+
+            if (req,user && post.author.id === req.user.id) {
+                return true;
+            }
+
+            return false;
+        })
+        res.send({ posts });
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+})
 
 module.exports = tagsRouter;

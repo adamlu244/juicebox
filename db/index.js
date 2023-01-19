7e744d235b8ac1389d1b335d42f846a974d2154c
Adam Lu <adamlu09@gmail.com>
2023-01-18 21:13:05 -0800

    post-commit 1

--- db/index.js
+++ db/index.js
@@ -45,11 +45,15 @@ async function updateUser(id, fields = {}) {
 }
 
 async function getAllUsers() {
-  const { rows } = await client.query(
-    `SELECT id, username, name, location, active
-    FROM users;`
-  );
-  return rows;
+  try {
+    const { rows } = await client.query(
+      `SELECT id, username, name, location, active
+      FROM users;`
+    );
+    return rows;
+  } catch(error) {
+    throw error;
+  }
 }
 
 async function getUserById(userId) {
